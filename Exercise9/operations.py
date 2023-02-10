6e64b5c059da75db9e50418f8be7c0f5783e1e21
Weronika <67145460+WeraGitHub@users.noreply.github.com>
2023-02-10 13:48:49 +0000

    Exercise 9 part I, II and III

---
+++ Exercise9/operations.py
@@ -0,0 +1,8 @@
+# use input function to get the value from the user
+var = input('Please enter a value: ')
+
+print('Your value in upper cases: ', var.upper())
+
+print('The length of your value', len(var))
+
+print('Is your value a decimal?', var.isdecimal())
