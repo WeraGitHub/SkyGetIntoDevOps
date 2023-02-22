e19895b970e0db8a40bbd3840d16373c85ebb756
Weronika <weronikalimberger@gmail.com>
2023-02-22 21:02:09 +0000

    files from 22nd of Feb

--- 20feb_oop/account.py
+++ 20feb_oop/account.py
@@ -1,12 +1,38 @@
 # create a blueprint (a class) for a bank account
 
 class Account:
-    numCreated = 0
+    number_of_accounts = 0
 
     def __init__(self, opening_amount):
         self._balance = opening_amount
-        Account.numCreated += 1
+        Account.number_of_accounts += 1
 
+    # ########### DATA #############
+    # getter
+    def get_balance(self):
+        return self._balance
+
+    # getter
+    def get_firstname(self):
+        return self._firstname
+
+    # setter
+    def set_firstname(self, firstname):
+        self._firstname = firstname
+
+    # properties style
+    # getter
+    @property
+    def lastname(self):
+        return self._lastname
+
+    # setter
+    @lastname.setter
+    def lastname(self, lastname):
+        self._lastname = lastname
+
+    # ####### Functionality ######
+    # behaviour / functionality
     def deposit(self, amount):
         self._balance += amount
         return
@@ -16,5 +42,6 @@ class Account:
             self._balance -= amount
         return
 
-    def get_balance(self):
-        return self._balance
+    # overloading of __str__ special method
+    def __str__(self):
+        return "Bank account has a balance of £" + str(self._balance)
--- 20feb_oop/bank_client.py
+++ 20feb_oop/bank_client.py
@@ -1,4 +1,6 @@
 from account import Account
+from saving_account import SavingAccount
+
 # from the module import the Class
 
 # bring it to life
@@ -10,24 +12,71 @@ bart_account = Account(20)
 
 # objects dot attributes (data)
 # object dot methods (behaviour, aka functions)
-lisa_balance = lisa_account.get_balance()
-bart_balance = bart_account.get_balance()
+def get_and_display_balances():
+    global lisa_balance, bart_balance
+    lisa_balance = lisa_account.get_balance()
+    bart_balance = bart_account.get_balance()
+    print(f'Lisa has £{lisa_balance} and Bart has £{bart_balance}.')
 
-print(f'Lisa has £{lisa_balance} and Bart has £{bart_balance}.')
+
+get_and_display_balances()
 
 lisa_account._balance = 1000
 print(lisa_account._balance)
 
-lisa_balance = lisa_account.get_balance()
-print(f'Lisa has £{lisa_balance} and Bart has £{bart_balance}.')
+get_and_display_balances()
 
 lisa_account.deposit(50)
 
-lisa_balance = lisa_account.get_balance()
-print(f'Lisa has £{lisa_balance} and Bart has £{bart_balance}.')
+get_and_display_balances()
 
 lisa_account.withdraw(50)
 bart_account.withdraw(-50)
-lisa_balance = lisa_account.get_balance()
-bart_balance = bart_account.get_balance()
-print(f'Lisa has £{lisa_balance} and Bart has £{bart_balance}.')
+
+get_and_display_balances()
+
+if hasattr(lisa_account, '__str__'):
+    print('Lisa has an __str__')
+    print(lisa_account)
+    print(type(lisa_account))
+
+x = 10
+y = 3
+
+if x > y:
+    print('x is larger')
+
+# if lisa_account > bart_account:
+#     print('?')
+
+word = 'Wednesday '
+print(word * 4)
+print(4 * word)
+print(x * y)
+print(3.5 * y)
+
+nums = [1, 2, 3, 4]
+if nums:
+    print("The list is NOT empty")
+
+if bart_account:
+    print("Bart's account is TRUE")
+
+print(lisa_account)
+print(bart_account)
+
+# using Java type getters and setters for data
+lisa_account.set_firstname('Lisa')
+print(str(lisa_account.get_firstname()))
+account_firstname = lisa_account.get_firstname()
+
+# using .net type properties for data
+lisa_account.lastname = 'Simpson'
+print(lisa_account.lastname)
+
+
+# instantiate some saving account objects
+lisa_saving_account = SavingAccount(250, 1.5)
+marge_saving_account = SavingAccount(500, 3)
+marge_saving_account.set_firstname('Marge')
+marge_saving_account.lastname = 'Simpson'
