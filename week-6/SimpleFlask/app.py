31443d4cf8eaa76043b2ca7570125162c102d504
Weronika <weronikalimberger@gmail.com>
2023-03-08 20:57:13 +0000

    Add few routes with functions attached in our app

--- week-6/SimpleFlask/app.py
+++ week-6/SimpleFlask/app.py
@@ -1,5 +1,5 @@
 # app.py is the main file responsible for running the app
-from flask import Flask
+from flask import Flask, Response, request
 
 # instantiate the Flask application object
 app = Flask(__name__)
@@ -9,6 +9,55 @@ app = Flask(__name__)
 @app.route('/')
 def hello_from_flask():
     return "Hello from FLASK"
+
+
+@app.route('/bye', methods=['GET'])  # don't have to specify methods if it's get, because it is a default
+def goodbye_from_flask():
+    return "Goodbye from FLASK"
+
+
+@app.route('/get/text')
+def get_text():
+    return Response("Hello from FLASK using RESPONSE object")
+
+
+# http post request only
+@app.route('/post/text', methods=['POST'])
+def post_text():
+    data_sent = request.data.decode('utf-8')
+    return Response("You posted this data to the Flask App: " + data_sent)
+
+
+@app.route('/dynamic/<word>')
+def say_word(word):
+    return word
+
+
+@app.route('/square/<int:number>')
+def square(number):
+    squared = number ** 2
+    line = "Your number squared is " + str(squared)
+    return line
+
+
+@app.route('/name/<name>')
+def say_hello(name):
+    return "Hello " + name
+
+
+@app.route('/namePage/<name>')
+def say_hello_name(name):
+    return """
+    <html>
+    <head>
+        <title>Sample - Flask routes</title>
+    </head>
+    <body>
+        <h1>Name page</h1>
+        <p>Hello {}!</p>
+    </body>
+    </html>
+    """.format(name)
 
 
 # if this script is invoked directly (rather than being imported), run the flask app
